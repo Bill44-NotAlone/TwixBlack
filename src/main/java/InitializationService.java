f45b5fece283d1e8015080cfcc26bf6e0aa1eed6
Bill44-NotAlone <denyc9@mail.ru>
2024-03-02 18:00:49 +0300

    add empty spring boot

---
+++ src/main/java/InitializationService.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+public class InitializationService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InitializationService.class, args);
+    }
+}
